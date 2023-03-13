b1282e7c4536872a15f494c429ed03de0e819d38
minenam <nhm6947@gmail.com>
2023-03-13 16:32:04 +0900

    feat: 알림톡라우터 URL 경로 설정

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const userGroupRouter = require('./usergroup.route');
+const alimtalkRouter = require('./alimtalk.route');
 
 router.use('/users', [userGroupRouter]);
+router.use('/', [alimtalkRouter]);
 
 router.get('/', (_req, res) => {
   res.send('정상적으로 요청되었습니다.');
