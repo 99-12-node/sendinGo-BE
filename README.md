c960caee9cb55324dcfd8e5a2eb936f2777309ba
minenam <nhm6947@gmail.com>
2023-03-13 10:53:15 +0900

    init: node, express 초기 개발 환경 설정

---
+++ README.md
@@ -0,0 +1,29 @@
+# SendinGo Backend API Server
+
+> 대량발송 마케팅 SaaS
+
+<div align="center">
+  <img src="https://img.shields.io/badge/node-16.17.0-339933?logo=node.js">
+</div>
+
+## 👨‍👩‍👧 작업 구성원
+
+| 이름   | 깃허브                      | 역할 |
+| ------ | --------------------------- | ---- |
+| 남혜민 | https://github.com/minenam  |      |
+| 설연주 | https://github.com/chanbokW |      |
+| 이승운 | https://github.com/gyus13   |      |
+
+## 1. 서비스 개요
+
+- 서비스 소개 1
+- 서비스 소개 2
+- 서비스 소개 3
+
+## 2. 구현 사항
+
+### 1. 기능 목록
+
+### 2. ERD
+
+# 참조문서
---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const { logger } = require('./src/middlewares/logger');
+const cookieparser = require('cookie-parser');
+const errorMiddleware = require('./src/middlewares/error.middleware');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const router = require('./src/routes');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieparser());
+
+app.use(
+  cors({
+    methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
+    origin: true, // 배포 후에 도메인 추가
+    credentials: true,
+    exposedHeaders: ['*', 'Authorization', 'Content-Type'],
+  })
+);
+
+app.use('/api', router);
+app.use(errorMiddleware);
+
+app.listen(port);
+logger.info(`${process.env.NODE_ENV} - API Server Listening At Port ${port}`);
