d89ab75fdf02eec9050376a6d74e1341403066db
minenam <nhm6947@gmail.com>
2023-03-13 19:13:19 +0900

    feat: 알림톡 전송 API 시연

--- src/api/routes/alimtalk.route.js
+++ src/api/routes/alimtalk.route.js
@@ -8,6 +8,8 @@ const instance = axios.create({
   headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
 });
 
+const COMPANY = 'sendigo';
+
 // API 호출을 위한 토큰 생성
 router.get('/talk/auth', async (req, res) => {
   const params = new URLSearchParams({ apikey: process.env.ALIGO_APIKEY });
@@ -19,7 +21,43 @@ router.get('/talk/auth', async (req, res) => {
       '/i/',
     params
   );
-  console.log(aligoRes);
+  return res.status(200).json({ data: aligoRes.data });
+});
+
+// 알림톡 보내기
+router.post('/talk/send', async (req, res) => {
+  const params = new URLSearchParams({
+    apikey: process.env.ALIGO_APIKEY,
+    userid: process.env.ALIGO_USERID,
+    token: process.env.ALIGO_TOKEN,
+    senderkey: process.env.ALIGO_SENDERKEY,
+    tpl_code: 'TM_2048',
+    sender: process.env.SENDER,
+    receiver_1: process.env.RECEIVER_1,
+    recvname_1: '홍길동',
+    subject_1: '제목',
+    message_1: `[${COMPANY}] 주문완료안내\n
+    □ 주문번호 : 123412314123\n
+    □ 배송지 : 어느구 어느동\n
+    □ 배송예정일 : 03월 10일 \n
+    □ 결제금액 : 10,000 원`,
+    failover: 'Y',
+    fsubject_1: '문자제목1',
+    fmessage_1: `[${COMPANY}] 주문완료안내\n
+    □ 주문번호 : 123412314123\n
+    □ 배송지 : 어느구 어느동\n
+    □ 배송예정일 : 03월 10일 \n
+    □ 결제금액 : 10,000 원`,
+    testMode: 'Y',
+  });
+
+  const aligoRes = await instance.post(
+    process.env.ALIGO_BASE_URL +
+      '/akv10/alimtalk/send/' +
+      process.env.ALIGO_AUTH_MIN +
+      '/i/',
+    params
+  );
   return res.status(201).json({ data: aligoRes.data });
 });
 
