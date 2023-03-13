1a5cbe0722587ed1f1927ae4adf81ad97276a87b
Ryeonjoo <syeonjoo@naver.com>
2023-03-14 00:43:17 +0900

    fix: user->client 수정

--- src/api/routes/client.route.js
+++ src/api/routes/client.route.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const ClientController = require('../controllers/client.controller');
+const clientController = new ClientController();
+
+router.post('/', clientController.createClient);
+router.get('/', clientController.checkClient);
+router.delete('/:clientId', clientController.deleteClient);
+
 module.exports = router;
