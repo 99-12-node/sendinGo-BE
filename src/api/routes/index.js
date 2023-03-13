00b8c7d9d3742a670085d074a7b22e7f1437236f
Leeseungwoon <seong3546@gmail.com>
2023-03-13 20:00:24 +0900

    fix: index.js 파일 내 customerRouter 경로 추가

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const userGroupRouter = require('./usergroup.route');
+const customerRouter = require('./customer.routes');
 
 router.use('/users', [userGroupRouter]);
+router.use('/customers', [customerRouter]);
 
 router.get('/', (_req, res) => {
   res.send('정상적으로 요청되었습니다.');
