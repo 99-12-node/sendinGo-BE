bda1dcdeedb170f07986ba291285c92e2f44c641
minenam <nhm6947@gmail.com>
2023-03-13 16:58:21 +0900

    fix: 다른 도메인 중복 파일 삭제

--- src/api/routes/usergroup.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const UserGroupController = require('../controllers/usergroup.controller');
-const userGroupController = new UserGroupController();
-
-router.post('/groups', userGroupController.createUserGroup);
-
-module.exports = router;
