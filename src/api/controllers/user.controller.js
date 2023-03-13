2d9da85407d5f37ff1ad00e335141e4fb5dd8124
Ryeonjoo <syeonjoo@naver.com>
2023-03-13 15:40:48 +0900

    feat: user.controller.js생성 후 유저 등록 기본 코드 작성

---
+++ src/api/controllers/user.controller.js
@@ -0,0 +1,29 @@
+const { logger } = require('../middlewares/logger');
+const UserService = require('../services/user.service');
+
+module.exports = class userController {
+  constructor() {
+    this.userService = new UserService();
+  }
+
+  // 유저 등록
+  createUser = async (req, res, next) => {
+    const { user, phondNumber } = req.body;
+
+    try {
+      await this.UserService.createUser({
+        user,
+        phondNumber,
+      });
+
+      return res.status(201).json({ message: '유저가 등록되었습니다.' });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  //유저 전체 조회
+  checkUser = async (req, res, next) => {};
+  //유저 삭제
+  deleteUser = async (req, res, next) => {};
+};
