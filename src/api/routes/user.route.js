53db3a9abe61a0ea10a42f05f986b4f425b35737
Ryeonjoo <syeonjoo@naver.com>
2023-03-14 00:44:33 +0900

    fix: client -> user 수정

--- src/api/routes/user.route.js
+++ src/api/routes/user.route.js
@@ -1,11 +1,4 @@
 const express = require('express');
 const router = express.Router();
 
-const UserController = require('../controllers/user.controller');
-const userController = new UserController();
-
-router.post('/', userController.createUser);
-router.get('/', userController.checkUser);
-router.delete('/:userId', userController.deleteUser);
-
 module.exports = router;
