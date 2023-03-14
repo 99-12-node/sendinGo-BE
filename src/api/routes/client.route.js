e13bfa5653ba7a73583cfa14c2e1d65f65f01a38
Ryeonjoo <syeonjoo@naver.com>
2023-03-14 21:00:07 +0900

    fix: 변수명칭 수정

--- src/api/routes/client.route.js
+++ src/api/routes/client.route.js
@@ -5,7 +5,7 @@ const ClientController = require('../controllers/client.controller');
 const clientController = new ClientController();
 
 router.post('/', clientController.createClient);
-router.get('/', clientController.checkClient);
+router.get('/', clientController.getAllClient);
 router.delete('/:clientId', clientController.deleteClient);
 
 module.exports = router;
