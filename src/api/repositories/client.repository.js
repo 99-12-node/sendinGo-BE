bdb3d9ecbffa5a5c583432f86bfe6a3a2770a1ef
Ryeonjoo <syeonjoo@naver.com>
2023-03-14 20:56:05 +0900

    fix: 전체 컬럼 수정

--- src/api/repositories/client.repository.js
+++ src/api/repositories/client.repository.js
@@ -3,23 +3,25 @@ const { Client } = require('../../db/models');
 module.exports = class ClientRepository {
   constructor() {}
   // 클라이언트 생성
-  createClient = async (client, phoneNumber) => {
-    const createData = await User.createClient({
-      client,
-      phoneNumber,
-    });
+  createClient = async (userId, clientName, contact) => {
+    const createData = await Client.createClient(userId, clientName, contact);
     return createData;
   };
 
   //클라이언트 전체 조회
-  checkClient = async () => {
-    const allData = await Client.findAll({});
+  getAllClient = async (userId) => {
+    const allData = await Client.findAll({
+      where: { userId },
+      attributes: ['clientid', 'clientName', 'contact', 'createdAt'],
+    });
     return allData;
   };
 
   //클라이언트 삭제
-  deleteClient = async () => {
-    const deleteData = await Client.destroy({});
+  deleteClient = async (clientId) => {
+    const deleteData = await Client.destroy({
+      where: { clientId },
+    });
     return deleteData;
   };
 };
