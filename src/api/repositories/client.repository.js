79bf8bcd4a4131f350a78f83289e0f41d0f5b743
Ryeonjoo <syeonjoo@naver.com>
2023-03-15 01:51:22 +0900

    fix: 불필요한 userId주석처리 및 logger 추가, 오타 수정

--- src/api/repositories/client.repository.js
+++ src/api/repositories/client.repository.js
@@ -1,25 +1,37 @@
-const { Client } = require('../../db/models');
+const { logger } = require('../../middlewares/logger');
+const { Clients } = require('../../db/models');
 
 module.exports = class ClientRepository {
   constructor() {}
   // 클라이언트 생성
-  createClient = async (userId, clientName, contact) => {
-    const createData = await Client.createClient(userId, clientName, contact);
+  createClient = async ({
+    //userId,
+    clientName,
+    contact,
+  }) => {
+    logger.info(`clientController.createclient Request`);
+    const createData = await Clients.create({
+      //userId,
+      clientName,
+      contact,
+    });
     return createData;
   };
 
   //클라이언트 전체 조회
-  getAllClient = async (userId) => {
-    const allData = await Client.findAll({
-      where: { userId },
-      attributes: ['clientid', 'clientName', 'contact', 'createdAt'],
+  getAllClient = async ({}) => {
+    //userId
+    const allData = await Clients.findAll({
+      attributes: ['clientId', 'clientName', 'contact'],
+      // where: { userId },
+      // attributes: ['clientid', 'clientName', 'contact', 'createdAt'],
     });
     return allData;
   };
 
   //클라이언트 삭제
-  deleteClient = async (clientId) => {
-    const deleteData = await Client.destroy({
+  deleteClient = async ({ clientId }) => {
+    const deleteData = await Clients.destroy({
       where: { clientId },
     });
     return deleteData;
