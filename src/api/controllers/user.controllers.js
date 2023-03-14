afe12391ca099372d05c2b181d1105d7ec8754fd
Leeseungwoon <seong3546@gmail.com>
2023-03-14 17:20:16 +0900

    feat:로그인 컨트롤러 추가

--- src/api/controllers/user.controllers.js
+++ src/api/controllers/user.controllers.js
@@ -18,6 +18,22 @@ class userCotroller {
 
     res.status(201).json({ message: '회원가입이 완료 되었습니다.' });
   };
+
+  loginController = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    const user = await this.userService.loginService({ email, password });
+
+    let expires = new Date();
+    expires.setMinutes(expires.getMinutes() + 60);
+
+    const token = jwt.sign({ userId: user.email }, KEY, { expiresIn: '1h' });
+
+    res.cookie('authorization', `Bearer ${token}`, {
+      expires: expires,
+    });
+    res.status(200).json({ message: '로그인이 정상적으로 처리되었습니다.' });
+  };
 }
 
 module.exports = userCotroller;
