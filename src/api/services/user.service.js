0a81db6995c8371bab72cb087a38963153e6a6c1
Leeseungwoon <seong3546@gmail.com>
2023-03-14 17:21:02 +0900

    feat: 로그인 서비스 추가

--- src/api/services/user.service.js
+++ src/api/services/user.service.js
@@ -1,4 +1,5 @@
 const userRepository = require('../repositories/user.repository');
+const { BadRequestError } = require('../../exceptions/errors');
 
 class userService {
   constructor() {
@@ -21,6 +22,19 @@ class userService {
       name,
       role,
     });
+
+    return;
+  };
+
+  loginService = async ({ email, password }) => {
+    const user = await this.userRepository.loginRepo({ email });
+
+    if (!user) {
+      throw new BadRequestError({ message: '이메일이 존재하지 않습니다.' });
+    }
+    if (password !== user.password) {
+      throw new BadRequestError({ message: '비밀번호가 일치하지 않습니다.' });
+    }
 
     return;
   };
