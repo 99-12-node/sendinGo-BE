82f2319379b1dd9ac60694551848b467a023c2b5
minenam <nhm6947@gmail.com>
2023-03-15 16:18:56 +0900

    refactor: 알림톡 전송, 결과 조회 API 3계층 routes 정리

--- src/api/routes/alimtalk.route.js
+++ src/api/routes/alimtalk.route.js
@@ -1,64 +1,37 @@
 const express = require('express');
 const router = express.Router();
-
-const axios = require('axios');
-
-const instance = axios.create({
-  baseURL: process.env.ALIGO_BASE_URL,
-  headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-});
-
-const COMPANY = 'sendigo';
+const { logger } = require('../../middlewares/logger');
+const AlimtalkController = require('../controllers/alimtalk.controller');
+const alimtalkController = new AlimtalkController();
 
 // API 호출을 위한 토큰 생성
-router.get('/talk/auth', async (req, res) => {
-  const params = new URLSearchParams({ apikey: process.env.ALIGO_APIKEY });
-  params.append('userid', String(process.env.ALIGO_USERID));
+router.get('/talk/auth', alimtalkController.generateSendToken);
+
+// 알림톡 보내기
+router.post('/talk/send', alimtalkController.sendAlimTalk);
+
+// 알림톡 전송 결과
+router.get('/talk/result', alimtalkController.getAlimTalkResult);
+
+// 알림톡 전송 결과 상세
+router.get('/talk/result/detail', async (req, res) => {
+  const params = new URLSearchParams({
+    apikey: process.env.ALIGO_APIKEY,
+    userid: process.env.ALIGO_USERID,
+    token: process.env.ALIGO_TOKEN,
+    mid: req.query.mid,
+    page: '1',
+    limit: '10',
+  });
+
   const aligoRes = await instance.post(
     process.env.ALIGO_BASE_URL +
-      '/akv10/token/create/' +
+      '/akv10/history/detail/' +
       process.env.ALIGO_AUTH_MIN +
       '/i/',
     params
   );
   return res.status(200).json({ data: aligoRes.data });
-});
-
-// 알림톡 보내기
-router.post('/talk/send', async (req, res) => {
-  const params = new URLSearchParams({
-    apikey: process.env.ALIGO_APIKEY,
-    userid: process.env.ALIGO_USERID,
-    token: process.env.ALIGO_TOKEN,
-    senderkey: process.env.ALIGO_SENDERKEY,
-    tpl_code: 'TM_2048',
-    sender: process.env.SENDER,
-    receiver_1: process.env.RECEIVER_1,
-    recvname_1: '홍길동',
-    subject_1: '제목',
-    message_1: `[${COMPANY}] 주문완료안내\n
-    □ 주문번호 : 123412314123\n
-    □ 배송지 : 어느구 어느동\n
-    □ 배송예정일 : 03월 10일 \n
-    □ 결제금액 : 10,000 원`,
-    failover: 'Y',
-    fsubject_1: '문자제목1',
-    fmessage_1: `[${COMPANY}] 주문완료안내\n
-    □ 주문번호 : 123412314123\n
-    □ 배송지 : 어느구 어느동\n
-    □ 배송예정일 : 03월 10일 \n
-    □ 결제금액 : 10,000 원`,
-    testMode: 'Y',
-  });
-
-  const aligoRes = await instance.post(
-    process.env.ALIGO_BASE_URL +
-      '/akv10/alimtalk/send/' +
-      process.env.ALIGO_AUTH_MIN +
-      '/i/',
-    params
-  );
-  return res.status(201).json({ data: aligoRes.data });
 });
 
 module.exports = router;
