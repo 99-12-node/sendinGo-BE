2bdc0445c519036e1d7bfee5907cfba200aca545
minenam <nhm6947@gmail.com>
2023-03-16 03:22:24 +0900

    refactor: 알림톡 전송, 결과  조회 API URL index로 묶음

--- src/api/routes/alimtalk.route.js
+++ src/api/routes/alimtalk.route.js
@@ -5,15 +5,15 @@ const AlimtalkController = require('../controllers/alimtalk.controller');
 const alimtalkController = new AlimtalkController();
 
 // API 호출을 위한 토큰 생성
-router.get('/talk/auth', alimtalkController.generateSendToken);
+router.get('/auth', alimtalkController.generateSendToken);
 
 // 알림톡 보내기
-router.post('/talk/send', alimtalkController.sendAlimTalk);
+router.post('/send', alimtalkController.sendAlimTalk);
 
 // 알림톡 전송 결과
-router.get('/talk/result', alimtalkController.getAlimTalkResult);
+router.get('/result', alimtalkController.getAlimTalkResult);
 
 // 알림톡 전송 결과 상세
-router.get('/talk/result/detail', alimtalkController.getAlimTalkDetailResult);
+router.get('/result/detail', alimtalkController.getAlimTalkDetailResult);
 
 module.exports = router;
--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const alimtalkRouter = require('./alimtalk.route');
 
-router.use('/', [alimtalkRouter]);
+router.use('/talk', [alimtalkRouter]);
 
 router.get('/', (_req, res) => {
   res.send('정상적으로 요청되었습니다.');
