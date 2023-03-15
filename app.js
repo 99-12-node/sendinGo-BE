272c7d073846c1935d6dc5f7794c460dee701dda
Ryeonjoo <syeonjoo@naver.com>
2023-03-15 18:31:05 +0900

    fix: client 생성, 조회, 삭제까지 복구

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cookieparser = require('cookie-parser');
 const errorMiddleware = require('./src/middlewares/error.middleware');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const router = require('./src/api/routes');
+const router = require('./src/routes');
 
 dotenv.config();
 
