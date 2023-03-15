bc5143f77e462a7caf56efb46d6d6f2825c8172e
Ryeonjoo <121853931+Ryeonjoo@users.noreply.github.com>
2023-03-15 13:11:28 +0900

    Revert "feat: migrations, models 폴더 생성 및 user 3계층 기본 코드 작성"

--- src/api/routes/clientgroup.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const ClientGroupController = require('../controllers/clientgroup.controller');
-const clientGroupController = new ClientGroupController();
-
-router.post('/groups', clientGroupController.createClientGroup);
-
-module.exports = router;
--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const clientRouter = require('./client.route');
-const clientGroupRouter = require('./clientgroup.route');
+const userGroupRouter = require('./usergroup.route');
 
-router.use('/clients', [clientRouter]);
-router.use('/clients', [clientGroupRouter]);
+router.use('/users', [userGroupRouter]);
 
 router.get('/', (_req, res) => {
   res.send('정상적으로 요청되었습니다.');
