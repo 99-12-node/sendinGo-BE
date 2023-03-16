0467dfc81fa15d427945b881421440ee02d00280
Leeseungwoon <seong3546@gmail.com>
2023-03-16 14:34:13 +0900

    fix: user.controller 코드 수정 및 에러핸들링 추가

--- src/api/controllers/user.controller.js
+++ src/api/controllers/user.controller.js
@@ -9,28 +9,36 @@ class UserCotroller {
   }
 
   createUser = async (req, res, next) => {
+    logger.info(`UserCotroller.createUser Request`);
     const user = req.body;
-    await this.userService.createUser(user);
-
-    res.status(201).json({ message: '회원가입이 완료 되었습니다.' });
+    try {
+      await this.userService.createUser(user);
+      res.status(201).json({ message: '회원가입이 완료 되었습니다.' });
+    } catch (error) {
+      next(error);
+    }
   };
 
   loginUser = async (req, res, next) => {
+    logger.info(`UserCotroller.loginUser Request`);
     const { email, password } = req.body;
+    try {
+      const user = await this.userService.loginUser({ email, password });
 
-    const user = await this.userService.loginUser({ email, password });
+      let expires = new Date();
+      expires.setMinutes(expires.getMinutes() + 60);
 
-    let expires = new Date();
-    expires.setMinutes(expires.getMinutes() + 60);
+      const token = jwt.sign({ userId: user.email }, KEY, {
+        expiresIn: EXPIRE_IN,
+      });
 
-    const token = jwt.sign({ userId: user.email }, KEY, {
-      expiresIn: EXPIRE_IN,
-    });
-
-    res.cookie('authorization', `Bearer ${token}`, {
-      expires: expires,
-    });
-    res.status(200).json({ message: '로그인이 정상적으로 처리되었습니다.' });
+      res.cookie('Authorization', `Bearer ${token}`, {
+        expires: expires,
+      });
+      res.status(200).json({ message: '로그인이 정상적으로 처리되었습니다.' });
+    } catch (error) {
+      next(error);
+    }
   };
 }
 
