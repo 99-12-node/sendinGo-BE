8dadccd062ab7cdaf6595f2462765ba15e9e630b
Leeseungwoon <seong3546@gmail.com>
2023-03-16 14:29:19 +0900

    feat

---
+++ src/middlewares/auth/auth.middleware.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+const { Users } = require('../../db/models');
+const { UnauthorizedError } = require('../../exceptions/errors');
+require('dotenv').config();
+const { KEY } = process.env;
+
+module.exports = async (req, res, next) => {
+  const { Authorization } = req.headers;
+  const [tokenType, token] = (Authorization ?? '').split(' ');
+  if (tokenType !== 'Bearer' || !token) {
+    throw new UnauthorizedError(
+      '토큰 타입이 일치하지 않거나, 토큰이 존재하지 않습니다.'
+    );
+  }
+  try {
+    const decodedToken = jwt.verify(token, KEY);
+    const userId = decodedToken.userId;
+    const user = await Users.findOne({ where: { email: userId } });
+    if (!user) {
+      throw new UnauthorizedError(
+        '토큰에 해당하는 사용자가 존재하지 않습니다.'
+      );
+    }
+
+    res.locals.user = user;
+    next();
+  } catch (error) {
+    res.clearCookie('Authorization'); //인증에 실패 할 경우 쿠키 삭제
+    next(error);
+  }
+};
