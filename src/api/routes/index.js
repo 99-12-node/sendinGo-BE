db23f9d99d028ae356c1bab363c9eda2842bf3b9
Leeseungwoon <seong3546@gmail.com>
2023-03-17 22:06:33 +0900

    fix: 코드 실행 문제로 userGroupRouter 주석처리

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const userGroupRouter = require('./usergroup.route');
+// const userGroupRouter = require('./usergroup.route');
 const userRouter = require('./user.route');
 const clientRouter = require('./client.route');
 const alimtalkRouter = require('./alimtalk.route');
 const groupRouter = require('./group.route');
 
 router.use('/talk', [alimtalkRouter]);
-router.use('/users', [userGroupRouter]);
+// router.use('/users', [userGroupRouter]);
 router.use('/clients', [clientRouter]);
 router.use('/users', [userRouter]);
 router.use('/groups', [groupRouter]);
