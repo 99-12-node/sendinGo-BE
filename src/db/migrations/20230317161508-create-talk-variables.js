1a9c9d4c6fbebde3dd2f4974807f27a1e29411cb
minenam <nhm6947@gmail.com>
2023-03-18 01:42:56 +0900

    feat: TalkVariables 테이블 migration, models 설계

---
+++ src/db/migrations/20230317161508-create-talk-variables.js
@@ -0,0 +1,29 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable(
+      'TalkVariables',
+      {
+        talkVariableId: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        talkVariableEng: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        talkVariableEng: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+      },
+      { timestamps: false }
+    );
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('TalkVariables');
+  },
+};
