b7c562dfbc181a5fe2bf55f385ad920bcc7dfec7
minenam <nhm6947@gmail.com>
2023-03-18 01:42:36 +0900

    feat: TalkTemplates 테이블 migration, models 설계

---
+++ src/db/models/talktemplates.js
@@ -0,0 +1,54 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class TalkTemplates extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.hasMany(models.TalkTemplates, {
+        sourceKey: 'talkTemplateId',
+        foreignKey: 'talkTemplateId',
+        onDelete: 'CASCADE',
+      });
+    }
+  }
+  TalkTemplates.init(
+    {
+      talkTemplateId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      talkTemplateCode: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      talkTemplateName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      talkTemplateContent: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.NOW,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.NOW,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'TalkTemplates',
+    }
+  );
+  return TalkTemplates;
+};
