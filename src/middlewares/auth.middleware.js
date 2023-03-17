71111d9163e2f009d9cdd857b3e566f3ebce5208
Leeseungwoon <seong3546@gmail.com>
2023-03-17 22:35:52 +0900

    fix: auth.middleware 코드 수정 where: email > userId

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,12 +1,13 @@
 const jwt = require('jsonwebtoken');
-const { Users } = require('../../db/models');
+const { Users } = require('../db/models');
 const { UnauthorizedError } = require('../exceptions/errors');
 require('dotenv').config();
 const { KEY } = process.env;
 
 module.exports = async (req, res, next) => {
-  const { Authorization } = req.cookies;
-  const [tokenType, token] = (Authorization ?? '').split(' ');
+  const { authorization } = req.cookies;
+  const [tokenType, token] = (authorization ?? '').split(' ');
+
   if (tokenType !== 'Bearer' || !token) {
     throw new UnauthorizedError(
       '토큰 타입이 일치하지 않거나, 토큰이 존재하지 않습니다.'
@@ -15,7 +16,7 @@ module.exports = async (req, res, next) => {
   try {
     const decodedToken = jwt.verify(token, KEY);
     const userId = decodedToken.userId;
-    const user = await Users.findOne({ where: { email: userId } });
+    const user = await Users.findOne({ where: { userId } });
     if (!user) {
       throw new UnauthorizedError(
         '토큰에 해당하는 사용자가 존재하지 않습니다.'
@@ -25,7 +26,7 @@ module.exports = async (req, res, next) => {
     res.locals.user = user;
     next();
   } catch (error) {
-    res.clearCookie('Authorization');
+    res.clearCookie('authorization');
     next(error);
   }
 };
