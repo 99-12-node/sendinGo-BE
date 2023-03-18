5f86ef932bae79901f2fbc4799e25c5aa5a030a7
Leeseungwoon <seong3546@gmail.com>
2023-03-18 15:25:25 +0900

    fix: routes/index.js 주석코드(userGroup) 제거

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-// const userGroupRouter = require('./usergroup.route');
 const userRouter = require('./user.route');
 const clientRouter = require('./client.route');
 const alimtalkRouter = require('./alimtalk.route');
 const groupRouter = require('./group.route');
 
 router.use('/talk', [alimtalkRouter]);
-// router.use('/users', [userGroupRouter]);
 router.use('/clients', [clientRouter]);
 router.use('/users', [userRouter]);
 router.use('/groups', [groupRouter]);
