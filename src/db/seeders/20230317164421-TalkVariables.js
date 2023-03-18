a48d35cd97cee069cc15a7831a670085ad25261a
minenam <nhm6947@gmail.com>
2023-03-18 17:28:40 +0900

    fix: 중복되는 컬럼 이름 수정

--- src/db/seeders/20230317164421-TalkVariables.js
+++ src/db/seeders/20230317164421-TalkVariables.js
@@ -5,7 +5,7 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('TalkVariables', [
       {
-        talkVariableEng: 'companyName',
+        talkVariableEng: 'organizationName',
         talkVariableKor: '회사명',
       },
       {
