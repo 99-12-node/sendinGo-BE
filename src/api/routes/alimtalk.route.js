22bb1bfb543797756ae12743e630cac057b7ff6e
minenam <nhm6947@gmail.com>
2023-03-19 11:47:37 +0900

    feat: 알림톡 전송 내용 저장 API Route

--- src/api/routes/alimtalk.route.js
+++ src/api/routes/alimtalk.route.js
@@ -7,11 +7,14 @@ const alimtalkController = new AlimtalkController();
 // API 호출을 위한 토큰 생성
 router.get('/auth', alimtalkController.generateSendToken);
 
+// 알림톡 전송 내용 일괄 저장
+router.post('/contents', alimtalkController.saveTalkContents);
+
 // 알림톡 보내기
-router.post('/send', alimtalkController.sendAlimTalk);
+router.post('/groups', alimtalkController.sendAlimTalk);
 
 // 알림톡 전송 결과
-router.get('/result', alimtalkController.getAlimTalkResult);
+router.get('/result/list', alimtalkController.getAlimTalkResult);
 
 // 알림톡 전송 결과 상세
 router.get('/result/detail', alimtalkController.getAlimTalkDetailResult);
