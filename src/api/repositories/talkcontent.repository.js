82c606c6969150c7e74e14de9b1b882665618584
minenam <nhm6947@gmail.com>
2023-03-19 11:49:05 +0900

    feat: talkContent Repository createTalkContent 추가

---
+++ src/api/repositories/talkcontent.repository.js
@@ -0,0 +1,20 @@
+const { logger } = require('../../middlewares/logger');
+const { Clients, TalkContents } = require('../../db/models');
+
+module.exports = class TalkContentRepository {
+  constructor() {}
+  // 톡 전송 내용 생성
+  createTalkContent = async ({ clientId, ...talkContentData }) => {
+    logger.info(`TalkContentRepository.createTalkContent Request`);
+    try {
+      const newTalkContent = await TalkContents.create({
+        clientId,
+        ...talkContentData,
+      });
+      return newTalkContent;
+    } catch (e) {
+      console.error(e);
+      throw new Error('전송 내용 저장에 실패하였습니다.');
+    }
+  };
+};
