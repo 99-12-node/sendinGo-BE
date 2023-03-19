3b1d7e169cb3a77b7e631c1087de9ca34225b86f
minenam <nhm6947@gmail.com>
2023-03-19 11:50:18 +0900

    feat: 알림톡 전송 내용 저장 controller

--- src/api/controllers/alimtalk.controller.js
+++ src/api/controllers/alimtalk.controller.js
@@ -1,9 +1,13 @@
 const { logger } = require('../../middlewares/logger');
 const AlimtalkService = require('../services/alimtalk.service');
+const ClientService = require('../services/client.service');
+const TalkTemplateService = require('../services/talktemplate.service');
 
 module.exports = class AlimtalkController {
   constructor() {
     this.alimtalkService = new AlimtalkService();
+    this.clientService = new ClientService();
+    this.talkTemplateService = new TalkTemplateService();
   }
   // 토큰 생성
   generateSendToken = async (_req, res, next) => {
@@ -11,6 +15,32 @@ module.exports = class AlimtalkController {
     try {
       const result = await this.alimtalkService.generateSendToken();
       return res.status(200).json({ data: result });
+    } catch (e) {
+      next(e);
+    }
+  };
+
+  // 알림톡 전송 내용 저장
+  saveTalkContents = async (req, res, next) => {
+    logger.info(`AlimtalkController.saveTalkContents`);
+    const { clientId, templateCode, ...talkContentData } = req.body;
+
+    try {
+      const confirm = await Promise.allSettled([
+        await this.clientService.getClientById({ clientId }),
+        await this.talkTemplateService.verifyTemplateData({
+          talkTemplateCode: templateCode,
+          ...talkContentData,
+        }),
+      ]);
+      if (confirm) {
+        // 알림톡 전송 내용 저장
+        const result = await this.alimtalkService.saveTalkContents({
+          clientId,
+          ...talkContentData,
+        });
+        return res.status(201).json(result);
+      }
     } catch (e) {
       next(e);
     }
