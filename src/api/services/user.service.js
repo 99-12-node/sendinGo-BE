4eb8974d2e412bd8b132964d01e15837fe6d5efb
minenam <nhm6947@gmail.com>
2023-03-28 13:09:01 +0900

    fix: UserService.createUser  에러 핸들링 삭제

--- src/api/services/user.service.js
+++ src/api/services/user.service.js
@@ -42,156 +42,128 @@ class UserService {
     name,
     role,
   }) => {
-    try {
-      logger.info(`UserService.createUser Request`);
-      let result;
-      const salt = await bcrypt.genSalt(SALT);
-      const hashedPassword = await bcrypt.hash(password, salt);
+    logger.info(`UserService.createUser Request`);
+    let result;
+    const salt = await bcrypt.genSalt(SALT);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-      const existCompany = await this.companyRepository.findCompanyByName({
+    const existCompany = await this.companyRepository.findCompanyByName({
+      companyName,
+    });
+
+    if (!existCompany) {
+      result = await this.userRepository.createNewUserAndCompany({
+        email,
+        password: hashedPassword,
         companyName,
+        companyNumber,
+        companyEmail,
+        phoneNumber,
+        provider: 0,
+        name,
+        role,
       });
-
-      if (!existCompany) {
-        result = await this.userRepository.createNewUserAndCompany({
-          email,
-          password: hashedPassword,
-          companyName,
-          companyNumber,
-          companyEmail,
-          phoneNumber,
-          provider: 0,
-          name,
-          role,
-        });
-      } else {
-        result = await this.userRepository.createUser({
-          email,
-          password: hashedPassword,
-          phoneNumber,
-          provider: 0,
-          name,
-          role,
-          companyId: existCompany.companyId,
-        });
-      }
-      if (!result) {
-        throw new BadRequestError(result);
-      }
-      return result;
-    } catch (e) {
-      console.error(e.errors);
-      throw new BadRequestError(
-        '입력값을 다시 확인해주세요. (가입 이메일이나 소속명, 소속 이메일은 필수이며 중복이 불가합니다.)'
-      );
+    } else {
+      result = await this.userRepository.createUser({
+        email,
+        password: hashedPassword,
+        phoneNumber,
+        provider: 0,
+        name,
+        role,
+        companyId: existCompany.companyId,
+      });
     }
+    return result;
   };
 
   checkUserEmail = async ({ email }) => {
     logger.info(`UserService.checkUserEmail Request`);
-    try {
-      const user = await this.userRepository.findUserByEmail({ email });
-
-      if (user) {
-        throw new Conflict('중복 된 이메일이 존재합니다.');
-      }
-
-      return;
-    } catch (e) {
-      next(e);
+    const user = await this.userRepository.findUserByEmail({ email });
+    if (user) {
+      throw new Conflict('중복 된 이메일이 존재합니다.');
     }
+    return user;
   };
 
   loginUser = async ({ email, password }) => {
     logger.info(`UserService.loginUser Request`);
-    try {
-      const user = await this.userRepository.findUserByEmail({ email });
+    const user = await this.userRepository.findUserByEmail({ email });
 
-      if (user === null) {
-        throw new BadRequestError('이메일이 존재하지 않습니다.');
-      }
-
-      const isPasswordCorrect = await bcrypt.compare(password, user.password);
-
-      if (!isPasswordCorrect) {
-        throw new BadRequestError('비밀번호가 일치하지 않습니다.');
-      }
-
-      return user;
-    } catch (e) {
-      next(e);
+    if (user === null) {
+      throw new BadRequestError('이메일이 존재하지 않습니다.');
     }
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordCorrect) {
+      throw new BadRequestError('비밀번호가 일치하지 않습니다.');
+    }
+
+    return user;
   };
 
   editUser = async (requestData) => {
     logger.info(`UserService.editUser Request`);
 
     const { user, updateInfo } = requestData;
-    try {
-      const findByUserId = await this.userRepository.findByUserId({
-        userId: user.userId,
-      });
-      if (!findByUserId) {
-        throw new NotFoundError('요청한 사용자 정보가 존재하지 않습니다.');
-      }
-
-      const salt = await bcrypt.genSalt(SALT);
-      const hashedPassword = await bcrypt.hash(updateInfo.password, salt);
-      if (user.role === 0) {
-        await this.userRepository.editUser({
-          userId: user.userId,
-          name: updateInfo.name,
-          email: updateInfo.email,
-          password: hashedPassword,
-          phoneNumber: updateInfo.phoneNumber,
-          role: updateInfo.role,
-        });
-        await this.companyRepository.editCompany({
-          companyId: user.companyId,
-          companyName: updateInfo.companyName,
-          companyNumber: updateInfo.companyNumber,
-          companyEmail: updateInfo.companyEmail,
-        });
-      } else {
-        await this.userRepository.editUser({
-          userId: user.userId,
-          name: updateInfo.name,
-          email: updateInfo.email,
-          password: hashedPassword,
-          phoneNumber: updateInfo.phoneNumber,
-          role: updateInfo.role,
-        });
-      }
-      return;
-    } catch (e) {
-      next(e);
+    const findByUserId = await this.userRepository.findByUserId({
+      userId: user.userId,
+    });
+    if (!findByUserId) {
+      throw new NotFoundError('요청한 사용자 정보가 존재하지 않습니다.');
     }
+
+    const salt = await bcrypt.genSalt(SALT);
+    const hashedPassword = await bcrypt.hash(updateInfo.password, salt);
+    if (user.role === 0) {
+      await this.userRepository.editUser({
+        userId: user.userId,
+        name: updateInfo.name,
+        email: updateInfo.email,
+        password: hashedPassword,
+        phoneNumber: updateInfo.phoneNumber,
+        role: updateInfo.role,
+      });
+      await this.companyRepository.editCompany({
+        companyId: user.companyId,
+        companyName: updateInfo.companyName,
+        companyNumber: updateInfo.companyNumber,
+        companyEmail: updateInfo.companyEmail,
+      });
+    } else {
+      await this.userRepository.editUser({
+        userId: user.userId,
+        name: updateInfo.name,
+        email: updateInfo.email,
+        password: hashedPassword,
+        phoneNumber: updateInfo.phoneNumber,
+        role: updateInfo.role,
+      });
+    }
+    return;
   };
 
   deleteUser = async (user) => {
     logger.info(`UserService.deleteUser Request`);
-    try {
-      const findByUserId = await this.userRepository.findByUserId({
-        userId: user.userId,
-      });
+    const findByUserId = await this.userRepository.findByUserId({
+      userId: user.userId,
+    });
 
-      if (!findByUserId) {
-        throw new NotFoundError('요청한 사용자 정보가 존재하지 않습니다.');
-      }
-
-      if (user.role === 0) {
-        await this.userRepository.deleteUser({ userId: user.userId });
-        await this.companyRepository.deleteCompany({
-          companyId: user.companyId,
-        });
-      } else {
-        await this.userRepository.deleteUser({ userId: user.userId });
-      }
-
-      return;
-    } catch (e) {
-      next(e);
+    if (!findByUserId) {
+      throw new NotFoundError('요청한 사용자 정보가 존재하지 않습니다.');
     }
+
+    if (user.role === 0) {
+      await this.userRepository.deleteUser({ userId: user.userId });
+      await this.companyRepository.deleteCompany({
+        companyId: user.companyId,
+      });
+    } else {
+      await this.userRepository.deleteUser({ userId: user.userId });
+    }
+
+    return;
   };
 }
 
