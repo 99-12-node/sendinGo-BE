e4d9f8112d9f14a4124c80610430fff49bbf7f0f
minenam <nhm6947@gmail.com>
2023-04-03 21:43:04 +0900

    refactor: TalkTemplate Seeder JSON 형식으로 변경

--- src/db/seeders/20230317164412-TalkTemplates.js
+++ src/db/seeders/20230317164412-TalkTemplates.js
@@ -32,7 +32,7 @@ module.exports = {
         text: `<span id="#{고객명}">#{고객명데이터}</span> 고객님! <span id="#{택배회사명}">#{택배회사명데이터}</span>입니다.
 <span id="#{택배배송시간}">#{택배배송시간데이터}</span> 택배를 배달할 예정입니다.
 등기번호(운송장번호) : <span id="#{송장번호}">#{송장번호데이터}</span>`,
-        reqData: `['#{고객명}', '#{택배회사명}', '#{택배배송시간}', '#{송장번호}']'`,
+        reqData: `["#{고객명}", "#{택배회사명}", "#{택배배송시간}", "#{송장번호}"]`,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
@@ -53,7 +53,7 @@ module.exports = {
 □ 배송지 : <span id="#{구/면}">#{구/면}</span> <span id="#{동/리}">#{동/리}</span>
 □ 배송예정일 : <span id="#{월일}">#{월일}</span>
 □ 결제금액 : <span id="#{결제금액}">#{결제금액}</span>원`,
-        reqData: `['#{회사명}','#{주문번호}','#{구/면}','#{동/리}','#{월일}','#{결제금액}']`,
+        reqData: `["#{회사명}","#{주문번호}","#{구/면}","#{동/리}","#{월일}","#{결제금액}"]`,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
@@ -69,7 +69,7 @@ module.exports = {
         text: `<span id="#{고객명}">#{고객명}</span>고객님 우리 회사와 거래하여 주셔서 감사합니다.
         구매하신 제품의 사용(이용)법을
         확인해 보세요.`,
-        reqData: `['#{고객명}']`,
+        reqData: `["#{고객명}"]`,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
@@ -81,7 +81,7 @@ module.exports = {
 구매확정 부탁드립니다.`,
         text: `<span id="#{고객명}">#{고객명}</span>님께서 주문하신 물품이 배송완료 되었습니다.
     구매확정 부탁드립니다.`,
-        reqData: `['#{고객명}']`,
+        reqData: `["#{고객명}"]`,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
@@ -95,7 +95,7 @@ module.exports = {
 #{회사명}에 회원가입 해주셔서 진심으로 감사드립니다~`,
         text: `안녕하세요. <span id="#{고객명}">#{고객명}</span>님!
     <span id="#{회사명}">#{회사명}</span>에 회원가입 해주셔서 진심으로 감사드립니다~`,
-        reqData: `['#{고객명}', '#{회사명}']`,
+        reqData: `["#{고객명}", "#{회사명}"]`,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
