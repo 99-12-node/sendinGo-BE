ad55e5a5a619ace0a108ea7c118a5d1894dd0329
minenam <nhm6947@gmail.com>
2023-04-05 15:19:03 +0900

    feat: TalkContents model, migrataion - uuid 컬럼 추가

--- src/db/models/talkcontents.js
+++ src/db/models/talkcontents.js
@@ -129,6 +129,10 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.STRING,
         allowNull: true,
       },
+      trackingUUID: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
       trackingUrl: {
         type: DataTypes.STRING,
         allowNull: true,
