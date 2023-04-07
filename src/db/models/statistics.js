85a9947441cb10a2c7373cf3d63014d1b838e516
minenam <nhm6947@gmail.com>
2023-04-08 08:14:29 +0900

    fix: Statistics model, migration 불필요한 관계 제거

--- src/db/models/statistics.js
+++ src/db/models/statistics.js
@@ -7,13 +7,7 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      this.belongsTo(models.TalkSends, {
-        targetKey: 'talkSendId',
-        foreignKey: 'talkSendId',
-        onDelete: 'CASCADE',
-      });
-    }
+    static associate(models) {}
   }
   Statistics.init(
     {
