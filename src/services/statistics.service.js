e216643e0970a36948e0145e6eb7514483103257
minenam <nhm6947@gmail.com>
2023-04-08 21:54:40 +0900

    feat: 일별 통계, 시간별 통계 저장 statistic Service 구현

--- src/services/statistics.service.js
+++ src/services/statistics.service.js
@@ -6,6 +6,78 @@ class StatisticsService {
     this.statisticsRepository = new StatisticsRepository();
   }
 
+  // 시간별 통계 생성
+  createHourlyStatistics = async ({ userId, companyId }) => {
+    logger.info(`StatisticsService.createHourlyStatistics Request`);
+    try {
+      // 전체 고객 수 조회
+      const totalClientCount =
+        await this.statisticsRepository.getTotalClientsCount({
+          userId,
+          companyId,
+        });
+
+      // 전체 그룹 수 조회
+      const totalGroupCount =
+        await this.statisticsRepository.getTotalGroupsCount({
+          userId,
+          companyId,
+        });
+
+      // 누적 총 발송 건수 조회
+      const accumulateSendCount =
+        await this.statisticsRepository.getAccumulateSendCount({
+          userId,
+          companyId,
+        });
+
+      // 누적 발송성공 건수 조회
+      const accumulateSuccessCount =
+        await this.statisticsRepository.getAccumulateSuccessCount({
+          userId,
+          companyId,
+        });
+
+      // 누적 발송성공률 계산
+      const accumulateSuccessRatio =
+        (accumulateSuccessCount / accumulateSendCount) * 100;
+
+      // 누적 클릭형 발송건수 조회
+      const accumulateClickCount =
+        await this.statisticsRepository.getAccumulateClickSendCount({
+          userId,
+          companyId,
+        });
+
+      // 누적 클릭 건수 조회
+      const accumulateClickSuccessCount =
+        await this.statisticsRepository.getAccumulateClickSuccessCount({
+          userId,
+          companyId,
+        });
+
+      // 누적 클릭률 계산
+      const accumulateClickRatio =
+        (accumulateClickSuccessCount.length / accumulateClickCount) * 100;
+
+      // 시간별 통계 생성
+      const hourlyStatistics =
+        await this.statisticsRepository.createHourlyStatistics({
+          totalClientCount,
+          totalGroupCount,
+          accumulateSendCount,
+          accumulateSuccessCount,
+          accumulateSuccessRatio,
+          accumulateClickRatio,
+        });
+
+      return hourlyStatistics;
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  // 일별 통계 생성
   createDailyStatistics = async ({ userId, companyId }) => {
     logger.info(`StatisticsService.createDailyStatistics Request`);
     try {
@@ -70,7 +142,7 @@ class StatisticsService {
 
       // 데일리 통계 생성
       const dailyStatistics =
-        await this.statisticsRepository.createDailyStatistics({
+        await this.statisticsRepository.createDaliyStatistics({
           userAfterJoinDate,
           totalClientCount,
           totalGroupCount,
