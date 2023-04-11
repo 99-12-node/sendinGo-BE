e9cab5936c5a6d31631332aa8bde23acba18417c
minenam <nhm6947@gmail.com>
2023-04-11 12:41:30 +0900

    refactor: 통계 유틸리지 중복 코드 제거

--- src/utils/statistic.schedule.js
+++ src/utils/statistic.schedule.js
@@ -1,44 +1,27 @@
 const schedule = require('node-schedule');
-const jwt = require('jsonwebtoken');
-const { Users, Companies } = require('../db/models');
-const { UnauthorizedError } = require('../exceptions/errors');
 const StatisticsService = require('../services/statistics.service');
+const statisticsService = new StatisticsService();
+
 const { logger } = require('../middlewares/logger');
 require('dotenv').config();
 const { KEY } = process.env;
-const statisticsService = new StatisticsService();
 
 module.exports = async (req, res, next) => {
-  logger.info(`daily.statistic.middleware Request`);
-  const { authorization } = req.headers;
-  const [tokenType, token] = (authorization ?? '').split(' ');
+  const { userId } = res.locals.user;
+  const { companyId } = res.locals.company;
 
   try {
-    if (!(tokenType && token)) {
-      next();
-    }
-    logger.info(`auth.TokenDecoded`);
-    const decodedToken = jwt.verify(token, KEY);
-    const { userId, companyId } = decodedToken;
-    const company = await Companies.findOne({ where: { companyId } });
-    const user = await Users.findOne({ where: { userId } });
-
-    if (!(user && company)) {
-      throw new UnauthorizedError(
-        '토큰에 해당하는 사용자 또는 소속이 존재하지 않습니다.'
-      );
-    }
-
-    logger.info(`createStatistics`);
-
-    const HOURLYRULE = '0 0 * * * *'; // 주기 : 매시 정각
-    const DAILYRULE = '0 0 0 * * *'; // 주기 : 매일 0시
+    logger.info(`utils createStatistics`);
+    const HOURLYRULE = '30 * * * * *'; // 주기 : 매시 정각
+    const DAILYRULE = '0 0 * * * *'; // 주기 : 매일 0시
     // 시간 단위
     const hourlyStatisticsJob = schedule.scheduleJob(HOURLYRULE, async () => {
+      logger.info(`> hour / scheduledJobs : ${hourlyStatisticsJob.name}`);
       await statisticsService.createHourlyStatistics({ userId, companyId });
     });
     // 일 단위
     const dailyStatisticsJob = schedule.scheduleJob(DAILYRULE, async () => {
+      logger.info(`> daily / scheduledJobs : ${dailyStatisticsJob.name}`);
       await statisticsService.createDailyStatistics({ userId, companyId });
     });
 
