a66cf9a56ebe33c2c93d29b42541f273c8146fcf
minenam <nhm6947@gmail.com>
2023-04-12 17:57:24 +0900

    feat: pm2 config 수정 - cluster 조정

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -8,5 +8,6 @@ npm install
 pm2 stop all
 pm2 kill
 
-pm2 start ecosystem.config.js --env production --only 'sendingo-app,sendingo-app-2'
+pm2 start ecosystem.config.js --env production --only sendingo-app --env PORT=3003
+pm2 start ecosystem.config.js --env production --only sendingo-app-2 --env PORT=3004
 # pm2 start ecosystem.config.js --env development --only sendingo-app-dev
