e657219f18f28469dbf5216c34a7283f06526f36
minenam <nhm6947@gmail.com>
2023-04-13 18:03:39 +0900

    refactor: 사용하지 않는 이전 코드 제거

--- src/services/talktemplate.service.js
+++ src/services/talktemplate.service.js
@@ -9,34 +9,6 @@ module.exports = class TalkTemplateService {
     this.talkTemplateRepository = new TalkTemplateRepository();
   }
 
-  // 템플릿 데이터 검증
-  verifyTemplateData = async ({ talkTemplateId, ...talkContentData }) => {
-    logger.info(`TalkTemplateService.verifyTemplateData`);
-    // 템플릿 코드로 템플릿 존재여부 확인
-    const existedTemplate = await this.talkTemplateRepository.getTemplateByCode(
-      {
-        talkTemplateId,
-      }
-    );
-    if (!existedTemplate) {
-      throw new NotFoundError('템플릿 조회에 실패하였습니다.');
-    }
-    // 해당 템플릿 변수들 불러오기
-    const variables =
-      await this.talkTemplateRepository.getVariablesByTemplateId({
-        talkTemplateId: existedTemplate.talkTemplateId,
-      });
-    // 입력 데이터와 템플릿 변수 일치여부 확인
-    const result = variables.every((value) => {
-      const currentVariable = value['talkVariableEng'];
-      const inputDataArray = Object.keys(talkContentData);
-      return inputDataArray.includes(currentVariable);
-    });
-    if (!result)
-      throw new BadRequestError('입력 데이터가 템플릿과 일치하지 않습니다.');
-    return result;
-  };
-
   // 템플릿 목록 전체 조회
   getTemplatesList = async () => {
     logger.info(`TalkTemplateService.getTemplatesList`);
