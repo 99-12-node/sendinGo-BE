e17fa765ddf3d69a87f2f83220f893ffdeb0b67e
minenam <nhm6947@gmail.com>
2023-04-17 21:49:08 +0900

    feat: 일별 및 주별 통계 조회 API - Service 구현

--- src/services/statistics.service.js
+++ src/services/statistics.service.js
@@ -163,6 +163,85 @@ class StatisticsService {
     }
   };
 
+  // 주별 통계 생성
+  createWeeklyStatistics = async ({ userId, companyId }) => {
+    logger.info(`StatisticsService.createWeeklyStatistics Request`);
+    try {
+      // 특정일자 기준 가입일로부터 지나온 일자 계산
+      const userJoinDate = await this.statisticsRepository.getJoinUser({
+        userId,
+        companyId,
+      });
+
+      // 전체 고객 수 조회
+      const totalClientCount =
+        await this.statisticsRepository.getTotalClientsCount({
+          userId,
+          companyId,
+        });
+
+      // 전체 그룹 수 조회
+      const totalGroupCount =
+        await this.statisticsRepository.getTotalGroupsCount({
+          userId,
+          companyId,
+        });
+
+      // 누적 총 발송 건수 조회
+      const accumulateSendCount =
+        await this.statisticsRepository.getAccumulateSendCount({
+          userId,
+          companyId,
+        });
+
+      // 누적 발송성공 건수 조회
+      const accumulateSuccessCount =
+        await this.statisticsRepository.getAccumulateSuccessCount({
+          userId,
+          companyId,
+        });
+
+      // 누적 발송성공률 계산
+      const accumulateSuccessRatio =
+        (accumulateSuccessCount / accumulateSendCount) * 100;
+
+      // 누적 클릭형 발송건수 조회
+      const accumulateClickCount =
+        await this.statisticsRepository.getAccumulateClickSendCount({
+          userId,
+          companyId,
+        });
+
+      // 누적 클릭 건수 조회
+      const accumulateClickSuccessCount =
+        await this.statisticsRepository.getAccumulateClickSuccessCount({
+          userId,
+          companyId,
+        });
+
+      // 누적 클릭률 계산
+      const accumulateClickRatio =
+        (accumulateClickSuccessCount.length / accumulateClickCount) * 100;
+
+      // 데일리 통계 생성
+      const weeklyStatistics =
+        await this.statisticsRepository.createWeeklyStatistics({
+          userId,
+          companyId,
+          totalClientCount,
+          totalGroupCount,
+          accumulateSendCount,
+          accumulateSuccessCount,
+          accumulateSuccessRatio,
+          accumulateClickRatio,
+        });
+
+      return weeklyStatistics;
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   // 가장 최신의 시간 기준 통계 조회
   getLatestHourlyStatistic = async ({ userId, companyId }) => {
     logger.info(`StatisticsService.getLatestHourlyStatistic Request`);
@@ -248,6 +327,32 @@ class StatisticsService {
     } catch (e) {
       console.error(e);
     }
+  };
+
+  // 일별 통계 조회
+  getDailyStatistic = async ({ userId, companyId }) => {
+    logger.info(`StatisticsService.getDailyStatistic Request`);
+    const dailyStatistics = await this.statisticsRepository.getDailyStatistic({
+      userId,
+      companyId,
+    });
+
+    console.log();
+
+    return dailyStatistics.map((data) => new CurrentStatisticsDto(data));
+  };
+
+  // 주별 통계 조회
+  getWeeklyStatistic = async ({ userId, companyId }) => {
+    logger.info(`StatisticsService.getWeeklyStatistic Request`);
+    const weeklyStatistics = await this.statisticsRepository.getWeeklyStatistic(
+      {
+        userId,
+        companyId,
+      }
+    );
+
+    return weeklyStatistics.map((data) => new CurrentStatisticsDto(data));
   };
 }
 
