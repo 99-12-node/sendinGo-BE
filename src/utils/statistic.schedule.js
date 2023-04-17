94a071ffb7fa2f91b75cca2b8dbba3002dac27a6
minenam <nhm6947@gmail.com>
2023-04-17 21:50:02 +0900

    feat: scheduler 작업 반복되지 않도록 로직 추가

--- src/utils/statistic.schedule.js
+++ src/utils/statistic.schedule.js
@@ -1,10 +1,10 @@
 const schedule = require('node-schedule');
+
 const StatisticsService = require('../services/statistics.service');
 const statisticsService = new StatisticsService();
 
 const { logger } = require('../middlewares/logger');
 require('dotenv').config();
-const { KEY } = process.env;
 
 module.exports = async (req, res, next) => {
   const { userId } = res.locals.user;
@@ -12,23 +12,31 @@ module.exports = async (req, res, next) => {
 
   try {
     logger.info(`utils createStatistics`);
-    const HOURLYRULE = '0 0 * * * *'; // 주기 : 매시 정각
-    const DAILYRULE = '0 0 0 * * *'; // 주기 : 매일 0시
-    // 시간 단위
-    const hourlyStatisticsJob = schedule.scheduleJob(HOURLYRULE, async () => {
-      logger.info(`> hour / scheduledJobs : ${hourlyStatisticsJob.name}`);
-      await statisticsService.createHourlyStatistics({ userId, companyId });
-    });
-    // 일 단위
-    const dailyStatisticsJob = schedule.scheduleJob(DAILYRULE, async () => {
-      logger.info(`> daily / scheduledJobs : ${dailyStatisticsJob.name}`);
-      await statisticsService.createDailyStatistics({ userId, companyId });
-    });
 
-    // 주 단위 TO DO
-    // const weeklytatisticsJob = schedule.scheduleJob(DAILYRULE, async () => {
-    //   await statisticsService.createHourlyStatistics({ userId, companyId });
-    // });
+    const statisticJob = `companyId:${companyId}:userId:${userId}`;
+    const HOURLYRULE = '0 0 * * * *'; // 주기 : 매시 정각
+    const DAILYRULE = '0 0 * * *'; // 주기 : 매일 0시
+    const WEEKLYRULE = '0 0 * * 1'; // 주기 : 매주 월 0시
+
+    if (!schedule.scheduleJobs[statisticJob]) {
+      // 시간 단위 통계 생성
+      const hourlyStatisticsJob = schedule.scheduleJob(HOURLYRULE, async () => {
+        logger.info(`> hour / scheduledJobs : ${hourlyStatisticsJob.name}`);
+        await statisticsService.createHourlyStatistics({ userId, companyId });
+      });
+      // 일 단위 통계 생성
+      const dailyStatisticsJob = schedule.scheduleJob(DAILYRULE, async () => {
+        logger.info(`> daily / scheduledJobs : ${dailyStatisticsJob.name}`);
+        await statisticsService.createDailyStatistics({ userId, companyId });
+      });
+
+      // 주 단위 통계 생성
+      const weeklytatisticsJob = schedule.scheduleJob(WEEKLYRULE, async () => {
+        logger.info(`> weekly / scheduledJobs : ${weeklytatisticsJob.name}`);
+        await statisticsService.createWeeklyStatistics({ userId, companyId });
+      });
+    }
+
     next();
   } catch (error) {
     next(error);
