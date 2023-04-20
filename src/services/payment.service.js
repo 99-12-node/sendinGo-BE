9cb7f413d374ea3d16c78ea826de6228add3ae33
minenam <nhm6947@gmail.com>
2023-04-20 22:36:02 +0900

    feat: 결제 도메인 Service 구현

---
+++ src/services/payment.service.js
@@ -0,0 +1,35 @@
+const { logger } = require('../middlewares/logger');
+const { BadRequestError } = require('../exceptions/errors');
+const PaymentRepository = require('../repositories/payment.repository');
+
+module.exports = class PaymentService {
+  constructor() {
+    this.paymentRepository = new PaymentRepository();
+  }
+  // 결제 내역 생성
+  createPayment = async ({ userId, companyId, ...paymentData }) => {
+    logger.info(`PaymentService.createPayment Request`);
+
+    const newPayment = await this.paymentRepository.createPayment({
+      userId,
+      companyId,
+      ...paymentData,
+    });
+    if (!newPayment) {
+      throw new BadRequestError('결제 내역 생성에 실패하였습니다.');
+    }
+
+    return newPayment;
+  };
+
+  // userId 로 결제 내역 조회
+  getPaymentsByUser = async ({ userId, companyId }) => {
+    logger.info(`PaymentService.getPaymentsByUser Request`);
+
+    const paymentList = await this.paymentRepository.getPaymentsByUser({
+      userId,
+      companyId,
+    });
+    return paymentList;
+  };
+};
